7e620071685f51d84b49ecb2b4a8873c257ac9ba
Alexis CHEMEL <chemel@users.noreply.github.com>
2021-04-12 19:33:04 +0200

    Doctrine migration upgrade

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
